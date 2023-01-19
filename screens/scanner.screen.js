f09b81c0467442a21caba542f1a609a5f3b5efa6
Pasan Madushan <pasanmadushan@gmail.com>
2023-01-19 20:09:20 +0530

    bar code scanner integrated

--- screens/scanner.screen.js
+++ screens/scanner.screen.js
@@ -1,49 +1,58 @@
 import React, {useState, useEffect} from 'react';
-import {Text, View, StyleSheet, Button, Linking} from 'react-native';
-// import { BarCodeScanner } from 'expo-barcode-scanner';
+import {Text, View, StyleSheet, Button} from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+import { useNavigation } from '@react-navigation/native';
 
 export default function Scanner(){
     const [hasPermission, setHasPermission] = useState(null);
-    // const [scanned, setScanned] = useState(false);
+    const [scanned, setScanned] = useState(false);
 
-    useEffect(()=>{
-        (async ()=>{
-            // const {status} = await BarCodeScanner.requestPermissionsAsync();
-            setHasPermission(status === 'granted');
-        })();
-    }, []);
+    const navigation = useNavigation();
 
+    useEffect(() => {
+    const getBarCodeScannerPermissions = async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    };
 
-    // const handleBarCodeScanned = ({type,data}) =>{
-    //     setScanned(true);
-    //     alert ("succesfully scanned!!");
-    // };
+    getBarCodeScannerPermissions();
+  }, []);
 
 
-    if (hasPermission === null){
-        return <Text> Requesting camera permissions </Text>
-    }
-    if (hasPermission ===false){
-        return <Text> No access to Camera </Text>
-    }
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+  };
 
-    return(
-        <View>
-            <BarCodeScanner
-                onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}   
-                style = { StyleSheet.absoluteFillObject } 
-            />
-            {scanned && <Button title='Tap to Scan Again' onPress={()=>setScanned(false)} />}
+    
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
 
-        </View>
-    )
+    return (
+    <View style={styles.container}>
+      <BarCodeScanner
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+        style={StyleSheet.absoluteFillObject}
+      />
+      {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
 
-
-
-
-
-
-
+      <Button title="back" onPress={()=>navigation.navigate('Home')}  />
 
 
+    </View>
+  );
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    height:'100%'
+  },
+
+});
