37d66c79e6a0fd1fcc8c36ee1e9f1c8e6afdbc31
Pasan Madushan <pasanmadushan@gmail.com>
2023-02-17 10:14:49 +0530

    Navigate to claim screen

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 import HomeScreen from "./screens/home.screen";
 import ScannerScreen from "./screens/scanner.screen";
 import ClaimScreen from "./screens/claim.screen";
@@ -12,9 +11,9 @@ const Stack = createStackNavigator();
 function App(){
   return (
     <Stack.Navigator screenOptions={{headerShown:false}}>
-      <Stack.Screen name="Claim" component={ClaimScreen}/>
       <Stack.Screen name="Home" component={HomeScreen}/>
       <Stack.Screen name="Scanner" component={ScannerScreen} />
+      <Stack.Screen name="Claim" component={ClaimScreen}/>
     </Stack.Navigator>
   )
 }
--- screens/scanner.screen.js
+++ screens/scanner.screen.js
@@ -21,7 +21,8 @@ export default function Scanner(){
 
   const handleBarCodeScanned = ({ type, data }) => {
     setScanned(true);
-    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+    // alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+    navigation.navigate('Claim');
   };
 
     
