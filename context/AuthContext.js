7edbb7bab8701521a2ea96b0c09e4ce16a2b25ce
Dineth <dineth.18@cse.mrt.ac.lk>
2023-02-18 09:37:08 +0530

    add axios

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -1,16 +1,26 @@
 import React, {createContext, useEffect, useState} from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from 'axios';
 
 export const AuthContext = createContext();
 export const AuthProvider = ({children}) => {
     const [isLoading, setIsLoading] = useState(true);
     const [userToken, setUserToken] = useState(null);
 
+    const setToken = () => {
+        console.log('blabla here')
+        axios.get(`http://localhost:3000/api/auth/login`).then((response) => {
+            console.log(response.data);
+        }).catch((err) => {
+            console.log(err);
+        })
+    }
     const login = () => {
         setIsLoading(true);
         setUserToken('dsfsds');
         AsyncStorage.setItem('userToken', 'dsfsds');
         setIsLoading(false);
+        setToken();
     }
 
     const logout = () => {
@@ -33,7 +43,10 @@ export const AuthProvider = ({children}) => {
 
     useEffect(() => {
         isLoggedIn();
-    }, [userToken]);
+        // logout();
+        // let userToken = AsyncStorage.getItem('userToken');
+        // console.log(userToken);
+    }, []);
 
     return (
         <AuthContext.Provider value={{login, logout, isLoading, userToken}} >
